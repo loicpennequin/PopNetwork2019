1e0f18f0e02c9413b20c149f630460883d595d45
Daria <lo.pennequin@gmail.com>
2019-01-11 09:50:08 +0100

    fixed SSR issue with PRovider component

--- src/client/components/Store/Provider.js
+++ src/client/components/Store/Provider.js
@@ -1,50 +1,45 @@
 // store/UserProvider.js
-import React, { createContext, useState, useEffect, memo } from 'react';
+import React, { createContext, useState, memo } from 'react';
 
 export const StoreContext = createContext({});
 
 // We need a reference to the state outside of the component to access it with hetState() otherwise we get the un-memoized verison of the state which is an empty object.
 let _state = {};
+const getState = () => _state;
+const _setState = newState => {
+    console.log(newState);
+    _state = { ..._state, ...newState };
+    setState(_state);
+};
 
 const RenderPure = memo(({ children }) => children);
 
+const makeStore = (props, state, setState) => {
+    const stateUpdateFns = Object.entries(props)
+        .filter(([key, value]) => {
+            return typeof value === 'function';
+        })
+        .reduce(
+            (acc, [key, value]) => ({
+                ...acc,
+                [key]: async (...args) => setState(await value(...args)(state))
+            }),
+            {}
+        );
+
+    const initialState = {
+        ...props,
+        ...stateUpdateFns,
+        setState: newState => setState(newState)
+    };
+    return initialState;
+};
+
 const Provider = props => {
-    const [state, setState] = useState(_state);
-    const [initialized, setInitialized] = useState(false);
-    const _setState = newState => {
-        console.log(newState);
-        _state = { ..._state, ...newState };
-        setState(_state);
-    };
-    const getState = () => _state;
+    const _setState = (...args) => setState(...args);
+    const [state, setState] = useState(makeStore(props, state, _setState));
 
-    const makeStore = () => {
-        const stateUpdateFns = Object.entries(props)
-            .filter(([key, value]) => {
-                return typeof value === 'function';
-            })
-            .reduce(
-                (acc, [key, value]) => ({
-                    ...acc,
-                    [key]: async (...args) => {
-                        console.log(key);
-                        return _setState(await value(...args)(state));
-                    }
-                }),
-                {}
-            );
-
-        const initialState = {
-            ...props,
-            ...stateUpdateFns,
-            setState: newState => _setState(newState)
-        };
-        _setState(initialState);
-        setInitialized(true);
-    };
-    useEffect(() => makeStore(), []);
-    // @FIXME probably not working with SSR. Needs fix later.
-    return !initialized ? null : (
+    return (
         <StoreContext.Provider value={state}>
             <RenderPure>{props.children}</RenderPure>
         </StoreContext.Provider>
